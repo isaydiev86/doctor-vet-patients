444684d58349fe49d4dada586db960e335a67b58
Ildan Saydiyev <i.saydiyev@uzum.com>
2024-11-27 17:19:12 +0500

    fix architecture

--- db/db.go
+++ db/db.go
@@ -5,3 +5,11 @@ import "github.com/jmoiron/sqlx"
 type DB struct {
 	*sqlx.DB
 }
+
+func New(cfg Config) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", cfg.dsn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
--- db/postgres/create_patient.go
+++ db/postgres/create_patient.go
@@ -6,8 +6,17 @@ import (
 	"doctor-vet-patients/internal/dto"
 )
 
-func (r *repo) CreatePatient(ctx context.Context, patient dto.Patient) error {
-	/// TODO в одной транзакции создать основные данные пациента и первоначальные данные для лечения
-	/// TODO это делать из бизнес логики или сразу тут ?
+func (d *DB) CreatePatient(ctx context.Context, patient dto.Patient) error {
+	query := `
+		INSERT INTO patient (fio, phone, address, animal, name, breed, gender, is_neutered)
+		VALUES (:fio, :phone, :address, :animal, :name, :breed, :gender, :is_neutered)
+		RETURNING id
+	`
+
+	_, err := d.db.NamedExecContext(ctx, query, patient)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
