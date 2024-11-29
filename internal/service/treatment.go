5315be04b7d54b74f25d9fe6af8f95f5edd0436d
Ildan Saydiyev <i.saydiyev@uzum.com>
2024-11-29 13:58:32 +0500

    fix bugs

--- internal/service/treatment.go
+++ internal/service/treatment.go
@@ -6,10 +6,10 @@ import (
 	"doctor-vet-patients/internal/dto"
 )
 
-func (s *Service) GetTreatments(ctx context.Context) ([]dto.Treatment, error) {
+func (s *Service) GetTreatments(ctx context.Context) ([]*dto.Treatment, error) {
 	return s.svc.DB.GetTreatments(ctx)
 }
 
-func (s *Service) GetTreatment(ctx context.Context, id int64) (dto.TreatmentDetail, error) {
+func (s *Service) GetTreatment(ctx context.Context, id int64) (*dto.TreatmentDetail, error) {
 	return s.svc.DB.GetTreatment(ctx, id)
 }
--- transport/interfaces.go
+++ transport/interfaces.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Services interface {
-	GetTreatments(ctx context.Context) ([]dto.Patient, error)
-	GetTreatment(ctx context.Context, id int64) (dto.TreatmentDetail, error)
+	GetTreatments(ctx context.Context) ([]*dto.Treatment, error)
+	GetTreatment(ctx context.Context, id int64) (*dto.TreatmentDetail, error)
 	CreatePatient(ctx context.Context, patient dto.Patient) error
 }
