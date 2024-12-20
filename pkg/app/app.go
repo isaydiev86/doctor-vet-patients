fdb37223c1bb607ec14c73288ac94d71ece89783
Ildan Saydiyev <i.saydiyev@uzum.com>
2024-12-20 14:02:00 +0500

    add error chan for start components

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -20,31 +20,43 @@ type Application struct {
 }
 
 func (a *Application) Run(ctx context.Context) error {
+	errChan := make(chan error, len(a.components))
+
 	for _, component := range a.components {
-		err := component.Start(ctx)
-		if err != nil {
-			return err
-		}
+		go func(c LifecycleComponent) {
+			a.logger.Info("Starting component:", c.string)
+			err := c.Start(ctx)
+			if err != nil {
+				a.logger.Error("Error starting component %s: %v", c.string, err)
+				errChan <- err
+			}
+		}(component)
 	}
 
-	a.waiting()
+	select {
+	case <-a.waiting():
+		a.logger.Info("App is stopping...")
+	case err := <-errChan:
+		a.logger.Error("Error occurred, stopping app: %v", err)
+		return err
+	}
 
 	for _, component := range a.components {
 		err := component.Stop(ctx)
 		if err != nil {
+			a.logger.Error("Error stopping component %s: %v", component.string, err)
 			return err
 		}
 	}
+
 	return nil
 }
 
-func (a *Application) waiting() {
+func (a *Application) waiting() <-chan os.Signal {
 	a.logger.Info("App started!")
 
 	wait := make(chan os.Signal, 1)
 	signal.Notify(wait, os.Interrupt, syscall.SIGTERM)
 
-	<-wait
-
-	a.logger.Info("App is stopping...")
+	return wait
 }
